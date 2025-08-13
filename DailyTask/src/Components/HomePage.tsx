26b377f47e51c6f7e291c7e0b2cf4083d2cf6f03
emotionallyUnstable <95876061+emotionallyUnstable@users.noreply.github.com>
2025-08-13 18:14:43 +0300

    Daily-1(main & optional achived)

--- DailyTask/src/Components/HomePage.tsx
+++ DailyTask/src/Components/HomePage.tsx
@@ -11,7 +11,7 @@ function HomePage() {
     <div className="h-screen flex items-center justify-center">
         <div className="flex flex-col gap-4 ">
             <button onClick={() => navigate("/maintask")} className="border-1 w-[120px]">Simple Form</button>
-            <button className="border-1 w-[120px]">Optional Form</button>
+            <button onClick={() => navigate("/optionaltask")} className="border-1 w-[120px]">Optional Form</button>
         </div>
     </div>
     </>
--- DailyTask/src/Components/OptionalForm.tsx
+++ DailyTask/src/Components/OptionalForm.tsx
@@ -1,14 +1,61 @@
-
+import { useState } from "react"
 
 
 function OptionalForm() {
+ const [email, setEmail] = useState("");
+ const [password, setPassword] = useState("");
+ const [isErrorEmail, setIsErrorEmail] = useState(false);
+ const [isErrorPassword, setIsErrorPassword] = useState(false);
+//  const [value, setValue] = useState("");
 
+const handleSubmit = (e: React.MouseEvent) => {
+  e.preventDefault();
+
+  let isValid = true;
+
+  if (password.length >= 6) {
+    setIsErrorPassword(false)
+  } else {
+    setIsErrorPassword(true);
+    isValid = false;
+  }
+  if (email.includes("@") && email.includes(".")) {
+    setIsErrorEmail(false)
+  } else {
+    setIsErrorEmail(true);
+    isValid = false;
+  }
+
+  if(isValid) {
+    alert("Welcome");
+  } else {
+    alert("Access Denied");
+  }
+
+}
 
   return (
     <>
       <div>
-        
-    
+
+        <div className="flex justify-center mt-[100px]">
+          <form className="border-1 flex flex-col w-[300px] h-[150px] gap-5" action="">
+
+            <div className="flex justify-around mt-[10px]">
+              <label htmlFor="email">Email:</label>
+              <input value={email} onChange={(e) => setEmail(e.target.value)} className={`border-1 ${isErrorEmail ? "border-red-500" : ""}`} type="email" id="email" required />
+            </div>
+
+            <div className="flex justify-around">
+              <label htmlFor="password" id="password">Password:</label>
+              <input value={password} onChange={(e) => setPassword(e.target.value)} className={`border-1 ${isErrorPassword ? "border-red-500" : ""}`} type="password" id="password" required />
+            </div>
+
+            <button onClick={handleSubmit} className="ml-[100px] border-1 w-[100px]">Sign In</button>
+
+          </form>
+        </div>
+  
       </div>
     </>
   )
